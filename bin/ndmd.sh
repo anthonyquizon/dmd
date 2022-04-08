678f8fa12ae3452f212e4a69c49506ade0c36310
Anthony Quizon <accounts@anthonyquizon.io>
2022-04-08 19:44:11 +1000

    ndmd

--- bin/ndmd.sh
+++ bin/ndmd.sh
@@ -1,1 +1,2 @@
+# run: ln -s $HOME/Development/vendor/dmd/bin/ndmd.sh /usr/local/bin/ndmd
 $HOME/Development/vendor/dmd/generated/osx/release/64/dmd  -conf=$HOME/Development/vendor/dmd/ini/empty/bin/dmd.conf -I$HOME/Development/vendor/druntime/import $@
