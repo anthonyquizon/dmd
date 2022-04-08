7fa043b3d50e64796fa994cb99badafc98e3aa11
Anthony Quizon <accounts@anthonyquizon.io>
2022-04-08 20:11:54 +1000

    ndmd runtime setup

--- bin/ndmd.sh
+++ bin/ndmd.sh
@@ -1,2 +1,7 @@
 # run: ln -s $HOME/Development/vendor/dmd/bin/ndmd.sh /usr/local/bin/ndmd
-$HOME/Development/vendor/dmd/generated/osx/release/64/dmd  -conf=$HOME/Development/vendor/dmd/ini/empty/bin/dmd.conf -I$HOME/Development/vendor/druntime/import $@
+$HOME/Development/vendor/dmd/generated/osx/release/64/dmd \
+    -conf=$HOME/Development/vendor/dmd/ini/antho/bin/dmd.conf \
+    -I$HOME/Development/vendor/druntime/import \
+    -L-L$HOME/Development/vendor/druntime/generated/osx/release/64 \
+    -L-ldruntime \
+    $@
