047b35f4f1f734015fbc24426a2bc74ea1c503f3
Anthony Quizon <accounts@anthonyquizon.io>
2022-04-08 20:13:02 +1000

    notes

--- bin/ndmd.sh
+++ bin/ndmd.sh
@@ -1,4 +1,6 @@
-# run: ln -s $HOME/Development/vendor/dmd/bin/ndmd.sh /usr/local/bin/ndmd
+# 1) download dmd runtime
+# 2) build dmd runtime -> run: make -f posix.mak
+# 3) run: ln -s $HOME/Development/vendor/dmd/bin/ndmd.sh /usr/local/bin/ndmd
 $HOME/Development/vendor/dmd/generated/osx/release/64/dmd \
     -conf=$HOME/Development/vendor/dmd/ini/antho/bin/dmd.conf \
     -I$HOME/Development/vendor/druntime/import \
